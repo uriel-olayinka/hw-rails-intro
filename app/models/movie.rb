fa322215bfa5a226c1a361da4213bed187105b71
Uriel Olayinka <umo204@nyu.edu>
2025-10-29 00:09:09 +0000

    Part 1: filtering list of movies by rating

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ApplicationRecord
+  def self.all_ratings
+    ['G', 'PG', 'PG-13', 'R']
+  end
+
+  def self.with_ratings(ratings_list)
+    if ratings_list.nil?
+      all
+    else
+      where(rating: ratings_list)
+    end
+  end
 end
